33ba55c6be0509caa1142ab9cbfe747f89b8365c
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2021-11-25 02:14:27 +0530

    Installed router concurrently and basic react setup

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -44,44 +44,56 @@ router.post('/addnote', fetchuser, [
 //ROUTE 3: Update Existing note of the user using: PUT "/api/notes/updatenote". Login Required
 router.put('/updatenote/:id', fetchuser, async (req, res) => {
     const { title, description, tag } = req.body;
-    
-    //Create a new Note object
-    const newNote = {};
-    if(title){newNote.title = title};
-    if(description){newNote.description = description};
-    if(tag){newNote.tag = tag};
 
-    //Find the note to be updated and update it
-    let note = await Note.findById(req.params.id);
-    
-    //If no note is present then send an error
-    if(!note){return res.status(404).send("Not Found")}
+    try {
+        //Create a new Note object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
 
-    //If user updating is not the owner of the note
-    if(note.user.toString()!==req.user.id){return res.status(401).send("Not Authorized");}
+        //Find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
 
-    //Update the new note
-    note = await Note.findByIdAndUpdate(req.params.id, {$set: newNote}, {new:true});
+        //If no note is present then send an error
+        if (!note) { return res.status(404).send("Not Found") }
 
-    res.json({note});
+        //If user updating is not the owner of the note
+        if (note.user.toString() !== req.user.id) { return res.status(401).send("Not Authorized"); }
+
+        //Update the new note
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+
+        res.json({ note });
+    } catch (error) {
+        //catch the unknown kind of error
+        console.log(error.message)
+        res.status(500).send("Internal Server Error")
+    }
 })
 
 //ROUTE 4: Delete note of the user using: DELETE "/api/notes/deletenote". Login Required
 router.delete('/deletenote/:id', fetchuser, async (req, res) => {
-
-    //Find the note to be deleted and delete it
+    try {
+        //Find the note to be deleted and delete it
     let note = await Note.findById(req.params.id);
-    
+
     //If no note is present then send an error
-    if(!note){return res.status(404).send("Not Found")}
+    if (!note) { return res.status(404).send("Not Found") }
 
     //If user deleteing is not the owner of the note
-    if(note.user.toString()!==req.user.id){return res.status(401).send("Not Authorized");}
+    if (note.user.toString() !== req.user.id) { return res.status(401).send("Not Authorized"); }
 
     //delete the new note
     note = await Note.findByIdAndDelete(req.params.id);
 
-    res.json({note});
+    res.json({ "Success": "Note has been deleted", note: note });
+    } catch (error) {
+        //catch the unknown kind of error
+        console.log(error.message)
+        res.status(500).send("Internal Server Error")
+    }
+    
 })
 
 module.exports = router
