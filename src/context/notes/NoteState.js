fe1684de0a3ef1f9a44aacf5342049b22ba59059
Lakshay-prog2503 <79397379+Lakshay-prog2503@users.noreply.github.com>
2021-11-26 01:06:30 +0530

    Made initial layout for HomePage

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,22 +2,10 @@ import NoteContext from "./noteContext";
 import React, { useState } from 'react';
 
 const NoteState = (props) => {
-    const [state, setstate] = useState({
-        "name" : "Lakshay",
-        "class" : "5b"
-    })
     
-    const update = () => {
-        setTimeout(()=>{
-            setstate({
-                "name" : "Harry",
-                "class" : "10b"
-            })
-        }, 1000)
-    }
 
     return (
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider >
             {props.children}
         </NoteContext.Provider>
     )
